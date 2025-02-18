2ff3d94e200ff2d7422bdfcb32a0d0e5384cbb60
eoinmtreacy <eointreacy5@gmail.com>
2025-02-18 18:55:26 +0000

    Add project files.

---
+++ test/ChatServiceTest/Test.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace ChatServiceTest;
+
+public class Test : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+
+    public Test(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task ChatService_Root_Returns_HelloWorld()
+    {
+        HttpRequestMessage req = new(HttpMethod.Get, "/");
+        var res = await _client.SendAsync(req);
+        var content = await res.Content.ReadAsStringAsync();
+
+        res.EnsureSuccessStatusCode();
+        Assert.Equal("Hello World!", content);
+    }
+}
