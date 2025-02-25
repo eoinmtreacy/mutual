06e8527abc59db70f70af7a8eb749f178c226b6c
Eoin Treacy <eointreacy5@gmail.com>
2025-02-25 08:15:43 +0000

    add logging to abstract client implementation

--- Client/AbstractClient.cs
+++ Client/AbstractClient.cs
@@ -5,32 +5,61 @@ namespace Client;
 
 public abstract class AbstractClient
 {
+    private readonly HubConnection Connection;
+    private readonly ILogger _logger;
     public string Username { get; }
 
-    private readonly HubConnection 	_connection;
 
-    public AbstractClient(string username)
+    public AbstractClient(string username, ILogger logger)
     {
-	Username = username;
-	_connection = new HubConnectionBuilder()
-	    .WithUrl("http://localhost:5250/chat")
-	    .WithAutomaticReconnect()
-	    .Build();
-	_connection.On<Message>("ReceiveMessage", ReceiveMessage);
-	StartConnection();
+        Username = username;
+        _logger = logger;
+        Connection = new HubConnectionBuilder()
+            .WithUrl("http://localhost:5250/chat")
+            .WithAutomaticReconnect()
+            .Build();
+        Connection.On<Message>("ReceiveMessage", ReceiveMessage);
+        Connection.Closed       += e => OnConnectionClosed(e);
+        Connection.Reconnecting += e => OnReconnecting(e);
+        Connection.Reconnected  += e => OnReconnected(e);
     }
 
-    public abstract void ReceiveMessage(Message m);
 
-    private void StartConnection()
+    public async Task SendMessage(Message m)
     {
-	    try { _connection.StartAsync(); }
-	    catch (Exception e) { Console.WriteLine($"Error connecting: {e}"); }
-    }
-
-    public async void SendMessage(Message m)
-    {
-        try { await _connection.InvokeAsync("SendMessage", m); }
+        try { await Connection.InvokeAsync("SendMessage", m); }
         catch { Console.WriteLine($"Error sending message: {m.Content}"); }
     }
+
+    protected virtual Task OnConnectionClosed(Exception? e)
+	{
+		_logger.LogError("Client '{Username}' connection closed - Error: {e}", Username, e);
+		return Task.CompletedTask;
+	}
+
+    protected virtual Task OnReconnecting(Exception? e)
+	{
+		_logger.LogError("Client '{Username}' reconnecting - Error: {e}", Username, e);
+		return Task.CompletedTask;
+	}
+
+    protected virtual Task OnReconnected(string? e)
+	{
+		_logger.LogInformation("Client '{Username}' reconnected - Connection Id: {e}", Username, e);
+		return Task.CompletedTask;
+	}
+    public async Task StartConnection()
+    {
+	    try 
+        { 
+            await Connection.StartAsync();
+            _logger.LogInformation("Client '{Username}' connected", Username);
+        }
+	    catch (Exception e) { 
+            _logger.LogError("Client '{Username}' failed to connect - Error: {e}", Username, e);
+        }
+    }
+
+    protected abstract void ReceiveMessage(Message m);
+
 }
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,7 +1,19 @@
 using Client;
 using Model;
 
-ConsoleClient c = new(args[0]);
+string username = args[0];
+
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+	builder
+		.AddConsole()
+		.SetMinimumLevel(LogLevel.Debug);
+});
+
+ILogger logger = loggerFactory.CreateLogger<Program>();
+
+ConsoleClient c = new(username, logger);
+await c.StartConnection();
 
 await Task.Run(async () =>
 {
@@ -12,19 +24,21 @@ await Task.Run(async () =>
 		do content = Console.ReadLine();
 			while (content == null);
 		Message M = new(c.Username, content, DateTime.Now);
-		try { c.SendMessage(M); }
+		try { await c.SendMessage(M); }
 		catch { Console.WriteLine($"Error sending message: {M}"); }
     }
 });
 
 await Task.Delay(-1);
 
-class ConsoleClient(string username) : AbstractClient(username)
+class ConsoleClient(string username, ILogger logger) : AbstractClient(username, logger)
 {
-	public override void ReceiveMessage(Message m)
+
+	protected override void ReceiveMessage(Message m)
 	{
 		Console.WriteLine($"{m.Sender}");
 		Console.WriteLine($"\t{m.Content}");
 		Console.WriteLine($"\t{m.Timestamp}");
 	}
+
 }
