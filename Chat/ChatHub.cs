2a3cfa6dd35d58668a0876b33de3e9671899dbd9
eoin m treacy <eointreacy5@gmail.com>
2025-02-27 14:55:11 +0000

    useful chathub logging

--- Chat/ChatHub.cs
+++ Chat/ChatHub.cs
@@ -3,16 +3,37 @@ using Model;
 
 public class ChatHub : Hub
 {
+    private readonly ILogger _logger;
+
+    public ChatHub(ILogger<ChatHub> logger) 
+    {
+	_logger = logger;
+    }
     public async Task SendMessage(string user, string message)
     {
-        Console.WriteLine(user + " " + message);
-        await Clients.All.SendAsync("ReceiveMessage", user, message);
+	try
+	{
+	    await Clients.All.SendAsync("ReceiveMessage", user, message);
+	    _logger.LogInformation("Sending message: '{}' from user: '{}'", message, user);
+	}
+	catch (Exception e)
+	{
+	    _logger.LogError("Error sending message: '{}' from user: '{}' -- {}", message, user, e);
+	}
     }
 
     public override Task OnConnectedAsync()
     {
-	Console.WriteLine($" Client: {Context.ConnectionId}, connected to ChatHub");
-	return base.OnConnectedAsync();
+	try
+	{
+	    _logger.LogInformation("Client: '{}', connecting to ChatHub", Context.ConnectionId);
+	    return base.OnConnectedAsync();
+	}
+	catch (Exception e)
+	{
+	    _logger.LogError("Error connecting client: '{}' to ChatHub", Context.ConnectionId, e);
+	    return Task.CompletedTask;
+	}
     }
 
 }
