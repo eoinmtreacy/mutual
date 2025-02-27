5bd780a14b417ad22254f07fdeabf0c90ad29003
eoin m treacy <eointreacy5@gmail.com>
2025-02-27 20:48:13 +0000

    event listeners so ui refreshes

--- Frontend/Frontend.Shared/Services/WebClient.cs
+++ Frontend/Frontend.Shared/Services/WebClient.cs
@@ -3,9 +3,10 @@ using Microsoft.Extensions.Logging;
 using Share;
 using Share.Model;
 
-public class WebClient : Client, IClient
+public class WebClient : Client
 {
     public List<Message> MessageList;
+    public event Action? OnMessageReceived;
 
     public WebClient(string url, string username, ILogger logger) : base(url, username, logger)
     {
@@ -14,6 +15,7 @@ public class WebClient : Client, IClient
     public override Task ReceiveMessage(Message M)
     {
         MessageList.Add(M);
+	OnMessageReceived?.Invoke();
         Logger.LogInformation("Message received: {}", M.Content);
 	    return Task.CompletedTask;
     }
