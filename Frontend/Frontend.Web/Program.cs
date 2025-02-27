08ac9ab6f41c981e8d04ce7f2f5c7ab5c46ae6e1
eoinmtreacy <77630567+eoinmtreacy@users.noreply.github.com>
2025-02-27 20:50:56 +0000

    Maui hybrid test (#6)
    succesful

---
+++ Frontend/Frontend.Web/Program.cs
@@ -0,0 +1,39 @@
+using Frontend.Web.Components;
+using Frontend.Shared.Services;
+using Frontend.Web.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// Add device-specific services used by the Frontend.Shared project
+builder.Services.AddSingleton<IFormFactor, FormFactor>();
+
+builder.Services.AddScoped<WebClient>(provider =>
+{
+    ILogger logger = provider.GetRequiredService<ILogger<WebClient>>();
+    return new WebClient("https://localhost:7088/chat", "eoin", logger);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddAdditionalAssemblies(typeof(Frontend.Shared._Imports).Assembly);
+
+app.Run();
