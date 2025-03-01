06ddef4b0db0c21cc214920a5baec6352c62cdf5
eoin m treacy <eointreacy5@gmail.com>
2025-03-01 14:30:40 +0000

    chathub namespaces

--- Chat/ChatHub.cs
+++ Chat/ChatHub.cs
@@ -2,48 +2,45 @@ using Microsoft.AspNetCore.SignalR;
 using Share;
 using Share.Model;
 
-public class ChatHub : Hub<IClient>
+namespace Chat;
+
+public class ChatHub(ILogger<ChatHub> logger) : Hub<IClient>
 {
-    private readonly ILogger _logger;
+    private readonly ILogger _logger = logger;
 
-    public ChatHub(ILogger<ChatHub> logger) 
+    public async Task SendMessage(Message message)
     {
-	_logger = logger;
-    }
-
-    public async Task SendMessage(Message M)
-    {
-	try
-	{
-	    await Clients.All.ReceiveMessage(M);
-	    _logger.LogInformation("Sending message: '{}' from user: '{}'", M.Content, M.Sender);
-	}
-	catch (Exception e)
-	{
-	    _logger.LogError("Error sending message: '{}' from user: '{}' -- Error: {}", M.Content, M.Sender, e);
-	}
+		try
+		{
+			await Clients.All.ReceiveMessage(message);
+			_logger.LogInformation("Sending message: '{}' from user: '{}'", message.Content, message.Sender); 
+		}
+		catch (Exception e)
+		{
+			_logger.LogError("Error sending message: '{}' from user: '{}' -- Error: {}", message.Content, message.Sender, e);
+		}
     }
 
     public override async Task OnConnectedAsync()
     {
-	try
-	{
-	    _logger.LogInformation("Client: '{}', connecting to ChatHub", Context.ConnectionId);
-	    await base.OnConnectedAsync();
-	}
-	catch (Exception e)
-	{
-	    _logger.LogError("Error connecting client: '{}' to ChatHub -- Error: {}", Context.ConnectionId, e);
-	}
+		try
+		{
+			_logger.LogInformation("Client: '{}', connecting to ChatHub", Context.ConnectionId);
+			await base.OnConnectedAsync();
+		}
+		catch (Exception e)
+		{
+			_logger.LogError("Error connecting client: '{}' to ChatHub -- Error: {}", Context.ConnectionId, e);
+		}
     }
 
     public override async Task OnDisconnectedAsync(Exception? e)
     {
-	if (e != null)
-	{
-	    _logger.LogError("Connection {} disconnected in error: {}", Context.ConnectionId, e);
-	}
-	await base.OnDisconnectedAsync(e);
+		if (e != null)
+		{
+			_logger.LogError("Connection {} disconnected in error: {}", Context.ConnectionId, e);
+		}
+		await base.OnDisconnectedAsync(e);
     }
 
 }
